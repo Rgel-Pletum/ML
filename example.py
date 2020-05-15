2ae5b40addf19e8a159186735b7f256ed3aa9da4
Vlad <topolev-tver@yandex.ru>
2020-05-15 22:43:54 +0300

    second

--- example.py
+++ example.py
@@ -1,43 +1,41 @@
 import numpy as np
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+data0 = pd.read_csv('titanic.csv', index_col='PassengerId')
+
+data=data0.dropna(how='any', subset=['Fare', 'Age','Pclass','Sex','Survived'], axis=0)
 
 
-
-data = pd.read_csv('titanic.csv', index_col='PassengerId')
-
-
-
-
-
-f=data['Sex']
-b=data['Name']
-b=data['Sex'].value_counts()
-print(b)
-res = data['Name'].get_values()
-list_name = []
-for row in res:
-	if ("Miss." in row):
-		i=row.find(".")
-		trname=row[i+2:]
-		i=trname.find(" ")
-		trname=trname[:i]
-		list_name.append(trname)
-	if ("Mrs." in row):
-		i = row.find("(")
-		trname = row[i + 1:]
-		i = trname.find(" ")
-		trname = trname[:i]
-		list_name.append(trname)
-print(list_name)
-dd = {'Name':list_name}
-df = pd.DataFrame(dd)
-print(df)
-b=df['Name'].value_counts()
-print(b)	
-
-
-
-
-
-
-
+Fare = data['Fare'].get_values()
+Age = data['Age'].get_values()
+Pclass = data['Pclass'].get_values()
+Sex0 = data['Sex'].get_values()
+Survived = data['Survived'].get_values()
+Sex=[]
+for gen in Sex0:
+	if (gen=='female'):
+		Sex.append(0)
+	else:
+		Sex.append(1)
+print(type(Fare))
+#print(Fare)
+print(Fare.shape)
+Fare = Fare.transpose()
+#print(Fare)
+print(Fare.shape)
+	
+Proper=np.vstack((Fare, Age))
+Proper=np.vstack((Proper, Pclass))
+Proper=np.vstack((Proper, Sex))
+print(Proper.shape)
+Proper=Proper.transpose()
+print(Proper.shape)
+#print(Proper)
+x = np.array(Proper)
+y = np.array(Survived)
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(x, y)
+importances = clf.feature_importances_
+print(importances)
+print(clf.predict([[10, 20, 2, 1]]))
--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-s='Pizdec'
-s1=s[3:]
-print(s1)
